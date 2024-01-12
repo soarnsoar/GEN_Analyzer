b229a7b50fca01b9a1f271e8a47e3c00288e7d13
jhchoi <soarnsoar@gmail.com>
2024-01-13 04:49:53 +0900

    rm init file

--- JHanalyzer/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/GEN_Analyzer/JHanalyzer/',1)[0])+'/cfipython/slc7_amd64_gcc700/GEN_Analyzer/JHanalyzer')
